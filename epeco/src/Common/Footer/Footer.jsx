00b9a607c89e2b3d27a57c0df640526288e8e70d
Yasmeen Mahmoud <yasmeen.mahmoud1610@gmail.com>
2026-07-18 15:33:54 +0300

    Handle footer UI and make it responsive

--- epeco/src/Common/Footer/Footer.jsx
+++ epeco/src/Common/Footer/Footer.jsx
@@ -1,38 +1,37 @@
+import { useTranslation } from "react-i18next";
+import { Link } from "react-router";
 
 export default function Footer() {
+
+  const {t} = useTranslation("footer");
     return (
       <>
-        <footer className="bg-neutral-primary-soft rounded-base shadow-xs m-4">
-          <div className="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between">
-            <ul className="flex flex-wrap items-center mt-3 text-sm font-medium text-body sm:mt-0">
+        <footer className="bg-[#032F0D] text-white border-[0.5px] border-[#5fcf8165]">
+          <div className="  w-full mx-auto max-w-screen-xl p-4  flex flex-col gap-2 md:flex md:flex-row md:items-center md:justify-between">
+            <ul className="flex flex-col  gap-2 md:flex-row md:flex-wrap items-center mt-3 text-sm font-medium text-body sm:mt-0">
               <li>
-                <a href="#" className="hover:underline me-4 md:me-6">
-                  About
-                </a>
+                <Link to="#" className="hover:underline me-4 md:me-6">
+                  {t("Contact")}
+                </Link>
               </li>
               <li>
-                <a href="#" className="hover:underline me-4 md:me-6">
-                  Privacy Policy
-                </a>
+                <Link to="#" className="hover:underline me-4 md:me-6">
+                  {t("terms")}
+                </Link>
               </li>
               <li>
-                <a href="#" className="hover:underline me-4 md:me-6">
-                  Licensing
-                </a>
-              </li>
-              <li>
-                <a href="#" className="hover:underline">
-                  Contact
-                </a>
+                <Link to="#" className="hover:underline me-4 md:me-6">
+                  {t("PrivacyPolicy")}
+                </Link>
               </li>
             </ul>
-            <span className="text-sm text-body sm:text-center">
-              © 2023{" "}
-              <a href="https://flowbite.com/" className="hover:underline">
-                Flowbite™
-              </a>
-              . All Rights Reserved.
-            </span>
+            <div className="flex items-center justify-center gap-2 font-RobotoCondensed">
+              <p className="text-sm text-body sm:text-center  ">
+                {t("description")}
+              </p>
+              <p className="text-sm text-body sm:text-center  "> EPECO</p>
+              <p className="text-sm text-body sm:text-center  ">2026© </p>
+            </div>
           </div>
         </footer>
       </>
